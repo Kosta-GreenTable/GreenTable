ba7077c146a9f5bc44a1713a2e832f8d2c722646
chan7674 <skc3924@gmail.com>
2025-05-15 17:09:57 +0900

    modify Listener

--- src/main/java/site/greentable/listener/HandlerMappingListener.java
+++ src/main/java/site/greentable/listener/HandlerMappingListener.java
@@ -9,6 +9,7 @@ import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 import site.greentable.controller.Controller;
+import site.greentable.controller.RestController;
 
 /**
  * Application Lifecycle Listener implementation class HandlerMappingListener
@@ -18,38 +19,73 @@ import site.greentable.controller.Controller;
 @WebListener
 public class HandlerMappingListener implements ServletContextListener {
 
-	public void contextInitialized(ServletContextEvent event) {
-
-		// actionMapping.properties파일 로딩
-		ResourceBundle rb = ResourceBundle.getBundle("actionMapping"); // resources/actionMapping.properties
-		
-		// 반복문 안헤서 key와 value를 분리해서
-
-		Map<String, Controller> map = new HashMap<String, Controller>();
-
-		try {
-			for (String key : rb.keySet()) {
-				String value = rb.getString(key);
-
-				// String은 value를 Reflection API를 적용해서 객체로 만든다.
-				Class<?> className = Class.forName(value);
-
-				// 생성 - value를 객체로 만들고
-				Controller con = (Controller) className.getDeclaredConstructor().newInstance();
-				System.out.println("key" + " = " + value + "con" + " = " + con);
-
-				// Map에 저장한다.
-				map.put(key, con);
-
-			} // for문끝
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		// 마지막에 map을 application영역에 저장한다.
-		ServletContext application = event.getServletContext();
-		application.setAttribute("map", map);
-		application.setAttribute("path", application.getContextPath()); // ${path}
-	}
-
+    public void contextInitialized(ServletContextEvent event) {
+        ServletContext application = event.getServletContext();
+        
+        // 일반 Controller 매핑 처리
+        Map<String, Controller> map = loadControllers("actionMapping");
+        application.setAttribute("map", map);
+        
+        // REST Controller 매핑 처리
+        Map<String, RestController> ajaxMap = loadRestControllers("ajaxMapping");
+        application.setAttribute("ajaxMap", ajaxMap);
+        
+        // 컨텍스트 경로 저장
+        application.setAttribute("path", application.getContextPath()); // ${path}
+    }
+    
+    /**
+     * 일반 Controller 객체를 로딩하는 메소드
+     */
+    private Map<String, Controller> loadControllers(String resourceName) {
+        Map<String, Controller> map = new HashMap<>();
+        
+        try {
+            // properties 파일 로딩
+            ResourceBundle rb = ResourceBundle.getBundle(resourceName); // resources/actionMapping.properties
+            
+            for (String key : rb.keySet()) {
+                String value = rb.getString(key);
+                
+                // Reflection API를 사용하여 객체 생성
+                Class<?> className = Class.forName(value);
+                Controller controller = (Controller) className.getDeclaredConstructor().newInstance();
+                System.out.println("Controller key: " + key + " = " + value);
+                
+                map.put(key, controller);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return map;
+    }
+    
+    /**
+     * RestController 객체를 로딩하는 메소드
+     */
+    private Map<String, RestController> loadRestControllers(String resourceName) {
+        Map<String, RestController> map = new HashMap<>();
+        
+        try {
+            // properties 파일 로딩 시도
+            ResourceBundle rb = ResourceBundle.getBundle(resourceName); // resources/ajaxMapping.properties
+            
+            for (String key : rb.keySet()) {
+                String value = rb.getString(key);
+                
+                // Reflection API를 사용하여 객체 생성
+                Class<?> className = Class.forName(value);
+                RestController controller = (RestController) className.getDeclaredConstructor().newInstance();
+                System.out.println("RestController key: " + key + " = " + value);
+                
+                map.put(key, controller);
+            }
+        } catch (Exception e) {
+            System.out.println("REST 컨트롤러 로딩 중 오류: " + e.getMessage());
+            // 리소스 번들을 찾지 못해도 프로그램은 계속 실행됨
+        }
+        
+        return map;
+    }
 }
