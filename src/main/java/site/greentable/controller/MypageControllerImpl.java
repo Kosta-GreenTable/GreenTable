ac4a86c13f5fa7a668bfb76a00e27f83ecafef59
chan7674 <skc3924@gmail.com>
2025-05-20 10:25:33 +0900

    modify user

--- src/main/java/site/greentable/controller/MypageControllerImpl.java
+++ src/main/java/site/greentable/controller/MypageControllerImpl.java
@@ -20,8 +20,4 @@ public class MypageControllerImpl implements Controller {
 	        return new ModelAndView("/user/mypage.jsp");
 	    }
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return mypage(request, response);
-	}
 }
